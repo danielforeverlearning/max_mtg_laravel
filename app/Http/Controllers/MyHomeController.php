9c90b21980a4d9931edd8e16904e40a2b067e124
Daniel Forever <danielforeverlearning@gmail.com>
2018-07-27 15:34:09 -0700

    shuffle
    shuffle repaired

--- app/Http/Controllers/MyHomeController.php
+++ app/Http/Controllers/MyHomeController.php
@@ -38,9 +38,87 @@ class MyHomeController extends Controller
 	  
 	  public function do_shuffle()
 	  {
-	      $rows = DB::connection('mysql')->select('select * from red_deck');
-	      $result = ["deck_name" => "red_deck", $rows];
-	      return view('shuffle', ['mydata' => $result]);
-	  }
+	        $rows = DB::connection('mysql')->select('select * from red_deck');
+	      
+	        $total_deck_count = 0;
+	        $ActualCards = array();
+	        for ($ii=0; $ii < count($rows); $ii++)
+	        {
+	          $total_deck_count += $rows[$ii]->card_count;
+	          for ($cc=0; $cc < $rows[$ii]->card_count; $cc++)
+	              array_push($ActualCards, $ii);
+	        }
+	      
+	        list($usec, $sec) = explode(' ', microtime());
+	        srand($sec + $usec * 1000000);
+	      
+	        $myrand = array();
+	        for ($ii = 0; $ii < $total_deck_count; $ii++)
+	          array_push($myrand, rand());
+	          
+	        //So now you got 2 arrays: ActualCards and myrandlist
+            //ok ok let us just do bubble-sort, i know it is O(n^2) ok ok,
+            //i am just lazy and tired to learn something new, please
+            //dang dude you could have just used List<int>.Sort() method gosh
+            $loopcount = 0;
+            $lastcardindex = $total_deck_count - 1;
+            $didswap = true;
+          
+            while ($didswap)
+            {
+                $didswap = false;
+                $loopcount++;
+                for ($cc = 0; $cc <= $lastcardindex; $cc++)
+                {
+                    if ($cc == $lastcardindex) //no pairs of cards left
+                    {
+                        break;
+                    }
 
-}
+                    $AA = $myrand[$cc];
+                    $BB = $myrand[$cc + 1];
+                    if ($BB < $AA)
+                    {
+                        $didswap = true;
+                        $myrand[$cc] = $BB;
+                        $myrand[$cc + 1] = $AA;
+
+                        $temp = $ActualCards[$cc];
+                        $ActualCards[$cc] = $ActualCards[$cc + 1];
+                        $ActualCards[$cc + 1] = $temp;
+                    }
+                }
+                $lastcardindex--;
+            }
+            
+            //printf("ActualCards count = %d", count($ActualCards));
+          
+            
+            //draw 7 cards
+            //bottom of ActualCards array is actually top of physical card deck
+            $hand = array();
+            $current_deck_count = $total_deck_count;
+            $actualcardsdrawncount = 0;
+            while ($actualcardsdrawncount < 7)
+            {
+                if ($current_deck_count > 0)
+                {
+                    $dcindex = $ActualCards[$current_deck_count - 1];
+                    
+                    //printf("dcindex = %d", $dcindex);
+                    
+                    $current_deck_count--;
+                    array_push($hand, $rows[$dcindex]);
+                    $actualcardsdrawncount++;
+                }
+                else
+                    break;
+            }
+            
+            $result = ["deck_name" => "red_deck", $hand];
+	        return view('shuffle', ['mydata' => $result]);
+	       
+	      
+	  }//do_shuffle
+
+}//class
--- resources/views/shuffle.blade.php
+++ resources/views/shuffle.blade.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <body>
-        <p>shuffle testing</p>
-        <p><?php printf("%s", $mydata['deck_name']); ?></p>
+        <p>My hand</p>
         <span>
         <?php
-            var_dump($mydata);
+            //var_dump($mydata);
             
-            //foreach ($mydata[0] as $row)
-            //{
-            //    printf("<img src='/laravel/resources/images/%s/%s' alt='i can not find this card' />", $mydata['deck_name'], $row->pic_filename);
-            //}
+            foreach ($mydata[0] as $row)
+            {
+                printf("<img src='/laravel/resources/images/%s/%s' alt='i can not find this card' />", $mydata['deck_name'], $row->pic_filename);
+            }
 		?>
 		</span>
     </body>
